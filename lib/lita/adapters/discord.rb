86c398452f9a98e71e7cf1fd43415c3af2a53f93
Kyle Boe <ksboe1@gmail.com>
2016-04-02 17:27:04 -0700

    added some boilerplate functions from https://docs.lita.io/plugin-authoring/adapters/

--- lib/lita/adapters/discord.rb
+++ lib/lita/adapters/discord.rb
@@ -4,18 +4,44 @@ module Lita
       config :email, type: String, required: true
       config :password, type: String, required: true
 
+      def initialize(robot)
+      	super
 
-      def mention_format(user_id)
-      	"@<#{user_id}>"
+      	## TIME TO REBUILD THIS SHIZZZZZZ
+      	@client = Discord::Client.new(config.email, config.password)
       end
+
+      def run
+      	@client.on_connect do
+					robot.trigger(:connected)      		
+      	end
+
+      	@client.on_message do |message, user, channel|
+      		user = Lita::User.find_by_name(user)
+      		user = Lita::User.create(user) unless user
+      		source = Lita::Source.new(user: user, room: channel)
+      		message = Lita::Message.new(robot, message, source)
+      		robot.receive(message)
+      	end
+
+      	@client.connect
+      end
+
+      def shut_down
+      	@client.disconnect
+      end
+
+      # def mention_format(user_id)
+      # 	"@<#{user_id}>"
+      # end
 
       # Referenced https://github.com/litaio/lita-slack/blob/master/lib/lita/adapters/slack.rb#L46
       # Trying to build out basic commands for max plugin compatibility 
       # Update when api is built out
-      def set_topic(target, topic)
+      # def set_topic(target, topic)
 
       	
-      end
+      # end
 
       Lita.register_adapter(:discord, self)
     end
