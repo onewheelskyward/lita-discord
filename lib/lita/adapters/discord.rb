d553e49bd239ccf7e269d4662e105c69ca7434c2
Kyle Boe <ksboe1@gmail.com>
2016-04-05 00:38:34 -0700

    building out more functionality, currently failing permanently after x attempts

--- lib/lita/adapters/discord.rb
+++ lib/lita/adapters/discord.rb
@@ -1,51 +1,38 @@
 require_relative "discord/client"
 
 module Lita
-  module Adapters
-    class Discord < Adapter
-      config :email, type: String, required: true
-      config :password, type: String, required: true
+	module Adapters
+		class Discord < Adapter
+			config :email, type: String, required: true
+			config :password, type: String, required: true
 
-      def initialize(robot)
-      	super
+			def initialize(robot)
+				super
+				## TIME TO REBUILD THIS SHIZZZZZZ
+				@client = Client.new(config.email, config.password)
+			end
 
-      	## TIME TO REBUILD THIS SHIZZZZZZ
-      	@client = Client.new(config.email, config.password)
-      end
+			def run
+				@client.connect  		
+			end
 
-     #  def run
-     #  	@client.on_connect do
-					# robot.trigger(:connected)      		
-     #  	end
+			def shut_down
+				@client.disconnect
+			end
 
-     #  	@client.on_message do |message, user, channel|
-     #  		user = Lita::User.find_by_name(user)
-     #  		user = Lita::User.create(user) unless user
-     #  		source = Lita::Source.new(user: user, room: channel)
-     #  		message = Lita::Message.new(robot, message, source)
-     #  		robot.receive(message)
-     #  	end
+			# def mention_format(user_id)
+			# 	"@<#{user_id}>"
+			# end
 
-     #  	@client.connect
-     #  end
+			# Referenced https://github.com/litaio/lita-slack/blob/master/lib/lita/adapters/slack.rb#L46
+			# Trying to build out basic commands for max plugin compatibility 
+			# Update when api is built out
+			# def set_topic(target, topic)
 
-      # def shut_down
-      # 	@client.disconnect
-      # end
 
-      # def mention_format(user_id)
-      # 	"@<#{user_id}>"
-      # end
+			# end
 
-      # Referenced https://github.com/litaio/lita-slack/blob/master/lib/lita/adapters/slack.rb#L46
-      # Trying to build out basic commands for max plugin compatibility 
-      # Update when api is built out
-      # def set_topic(target, topic)
-
-      	
-      # end
-
-      Lita.register_adapter(:discord, self)
-    end
-  end
+			Lita.register_adapter(:discord, self)
+		end
+	end
 end
--- lib/lita/adapters/discord/client.rb
+++ lib/lita/adapters/discord/client.rb
@@ -2,12 +2,84 @@ require 'rest-client'
 require 'faye/websocket'
 require 'eventmachine'
 require_relative 'api'
+require_relative 'token_cache'
 
 module Lita
 	module Adapters
 		class Discord < Adapter
 			class Client
+				def initialize(email, password)
+					@email = email
+					@password = password
+					@token_cache = TokenCache.new
+					@channels = {}
+					@users = {}
+					@restricted_channels = []
+					@event_threads = []
+					@current_thread = 0
+				end
 
+				def connect
+					@token = login
+				end
+
+				def disconnect
+					API.logout
+				end
+
+
+				private
+
+				def login
+					if @email == :token
+						Lita.logger.debug('Logging in using static token')
+						# The password is the token!
+						return @password
+					end
+
+					Lita.logger.debug('Logging in')
+					login_attempts ||= 0
+
+					# First, attempt to get the token from the cache
+					token = @token_cache.token(@email, @password)
+					if token
+						Lita.logger.debug('Token successfully obtained from cache!')
+						return token
+					end
+
+					# Login
+					login_response = API.login(@email, @password)
+					raise Discordrb::Errors::HTTPStatusError, login_response.code if login_response.code >= 400
+
+					# Parse response
+					login_response_object = JSON.parse(login_response)
+					raise Discordrb::Errors::InvalidAuthenticationError unless login_response_object['token']
+
+					Lita.logger.debug('Received token from Discord!')
+
+					# Cache the token
+					@token_cache.store_token(@email, @password, login_response_object['token'])
+
+					login_response_object['token']
+					rescue Exception => e
+					response_code = login_response.nil? ? 0 : login_response.code ######## mackmm145
+					if login_attempts < 100 && (e.inspect.include?('No such host is known.') || response_code == 523)
+						Lita.logger.debug("Login failed! Reattempting in 5 seconds. #{100 - login_attempts} attempts remaining.")
+						Lita.logger.debug("Error was: #{e.inspect}")
+						sleep 5
+						login_attempts += 1
+						retry
+					else
+						Lita.logger.debug("Login failed permanently after #{login_attempts + 1} attempts")
+						# Apparently we get a 400 if the password or username is incorrect. In that case, tell the user
+						Lita.logger.debug("Are you sure you're using the correct username and password?") if e.class == RestClient::BadRequest
+						# raise $ERROR_INFO
+					end
+				end
+
+				def logout
+					
+				end
 			end
 		end
 	end
